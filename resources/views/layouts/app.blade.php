3109aa66197b8790791bccea09a8ed5d6b18d7e3
Thiago Melo <4thiagomelo5@gmail.com>
2023-04-21 17:16:34 -0300

    build: add arquivo base

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- PWA -->
+    <meta name="theme-color" content="#6777ef" />
+    <link rel="apple-touch-icon" href="{{ asset('icon-512x512.png') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
+    <!-- PWA -->
+
+    @vite('resources/css/app.css')
+
+    @yield('css')
+
+    <title>App Coleta</title>
+</head>
+
+<body>
+    @yield('content')
+    {{-- <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        // Initialize the service worker
+                if ('serviceWorker' in navigator) {
+                navigator.serviceWorker.register('/sw.js', {
+                scope: '.'
+                }).then(function (registration) {
+                // Registration was successful
+                console.log('Laravel PWA: ServiceWorker registration successful with scope: ', registration.scope);
+                }, function (err) {
+                // registration failed :(
+                console.log('Laravel PWA: ServiceWorker registration failed: ', err);
+                });
+                }
+    </script> --}}
+    @yield('scripts')
+</body>
+
+</html>
